3c7d56064f2d44f10256b4159f306375cccb5899
Maciek Palmowski <m.palmowski@freshpixels.pl>
2023-02-24 00:44:08 +0100

    minor changes

--- tests/bike.spec.js
+++ tests/bike.spec.js
@@ -3,6 +3,6 @@ import { test, expect } from '@playwright/test';
 test('Is bike available?', async ({ page }) => {
   await page.goto( process.env.URL );
 
-  const bike = await page.getByTestId(process.env.SIZE);
+  const bike = await page.locator('[data-product-size="'+ process.env.SIZE +'"]');
   await expect(bike).toHaveClass(/js-nonSelectableVariation/);
 });
